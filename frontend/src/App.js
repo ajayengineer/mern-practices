1277c3556fba00827eb5962518038500390e3d09
ajayengineer <ajaywebsolution@gmail.com>
2023-01-28 15:39:49 -0800

    fronend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contact from './Components/Contact';
 import FetchData from './Components/FetchData';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Welcome to First Mern Practice Website - by <span className='text-primary'>Ajay Rai</span></h1>
       <FetchData />
+      <Contact />
     </div>
   );
 }
